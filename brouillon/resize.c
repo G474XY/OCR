93b405acaa74355e7d7a2612803424f7050c7aad
Jacques2904 <97622822+Jacques2904@users.noreply.github.com>
2022-11-08 00:31:32 +0100

    Update resize.c

--- brouillon/resize.c
+++ brouillon/resize.c
@@ -5,8 +5,12 @@
 
 SDL_Surface* crop_surface(SDL_Surface* sprite_sheet)
 {
+    int h = sprite_sheet->h;
+    int w = sprite_sheet->w;
+    int visited[h*w];
+    int result[h*w];
     int values[4];
-    grid_coordinates(sprite_sheet, values);
+    grid_coordinates(sprite_sheet, values, visited, result);
     int x = values[0];
     int y = values[1];
     int width = values[2] - values[0];
@@ -17,44 +21,44 @@ SDL_Surface* crop_surface(SDL_Surface* sprite_sheet)
     return surface;
 }
 
-int visited[h*w];
-int result[h*w];
-
 int COUNT = 0;
 
 // Function checks if a cell is valid
-bool is_valid(int x, int y, SDL_Surface* surface)
+int is_valid(int x, int y, int key, SDL_Surface* surface, int visited[])
 {
   SDL_PixelFormat* format = surface->format;
   int h = surface->h;
   int w = surface->w;
   Uint8 r, g, b;
   Uint32* pixels = surface->pixels;
-  
   SDL_GetRGB(pixels[x*(w)+y], format, &r, &g, &b);
 	if (x < h && y < w && x >= 0 && y >= 0)
 	{
-		if (visited[x*w+y] == 0 && r == 0)
+		if (visited[x*w+y] == 0 && r == key)
 		{
-			return true;
+			return 1;
 		}
 		else
 		{
-			return false;
+			return 0;
 		}
 	}
 	else
 	{
-		return false;
+		return 0;
 	}
 }
 
 // BFS to find all cells in
 // connection with key = input[i][j]
-void BFS (int x, int y, int i, int j, SDL_Surface* surface)
+void BFS (int x, int y, int i, int j, SDL_Surface* surface, int visited[])
 {
-  int h = surface->h;
-  int w = surface->w;
+  	SDL_PixelFormat* format = surface->format;
+  	int h = surface->h;
+  	int w = surface->w;
+ 	Uint8 r, g, b;
+  	Uint32* pixels = surface->pixels;
+  	SDL_GetRGB(pixels[x*(w)+y], format, &r, &g, &b);
 	// terminating case for BFS
 	if (x != y)
 	{
@@ -71,7 +75,7 @@ void BFS (int x, int y, int i, int j, SDL_Surface* surface)
 	// connected with input[i][j]
 	for (int k = 0; k < 4; k++)
 	{
-		if ((is_valid(i + y_move[k], j + x_move[k], x, surface)) == true)
+		if ((is_valid(i + y_move[k], j + x_move[k], pixels[i*w+j], surface)) == 1)
 		{
 			BFS(x, y, i + y_move[k], j + x_move[k], surface);
 		}
@@ -79,10 +83,10 @@ void BFS (int x, int y, int i, int j, SDL_Surface* surface)
 }
 
 
-void reset_visited(SDL_Surface* surface)
+void reset_visited(SDL_Surface* surface, int visited[])
 {
-  int h = surface->h;
-  int w = surface->w;
+  	int h = surface->h;
+  	int w = surface->w;
 	for (int i = 0; i < h; i++)
 	{
 		for (int j = 0; j < w; j++)
@@ -95,102 +99,103 @@ void reset_visited(SDL_Surface* surface)
 // If a larger connected component is
 // found this function is called to
 // store information about that component.
-void reset_result(SDL_Surface* surface)
+void reset_result(int key, SDL_Surface* surface, int visited[], int result[])
 {
   SDL_PixelFormat* format = surface->format;
   int h = surface->h;
   int w = surface->w;
   Uint32* pixels = surface->pixels;
 
-	for (int i = 0; i < h; i++)
+  for (int i = 0; i < h; i++)
+  {
+	for (int j = 0; j < w; j++)
 	{
-		for (int j = 0; j < w; j++)
+      		Uint8 r, g, b;
+      		SDL_GetRGB(pixels[i*(w)+j], format, &r, &g, &b);
+		if (visited[i*w+j] == 1 && r == key)
 		{
-      Uint8 r, g, b;
-      SDL_GetRGB(pixels[i*(w)+j], format, &r, &g, &b);
-			if (visited[i*w+j] == 1 && r == 0)
-			{
-				result[i*w+j] = visited[i*w+j];
-			}
-			else
-			{
-				result[i*w+j] = 0;
-			}
+			result[i*w+j] = visited[i*w+j];
+		}
+		else
+		{
+			result[i*w+j] = 0;
 		}
 	}
+    }
 }
 
 // function to calculate the
 // largest connected component
 // and return the coodinates of the component
-void grid_coordinates(SDL_Surface* surface, int[] values)
+void grid_coordinates(SDL_Surface* surface, int[] values, int visited[], int result[])
 {
-	int current_max = 0;
+  int current_max = -1;
   
   SDL_PixelFormat* format = surface->format;
   int h = surface->h;
   int w = surface->w;
   Uint32* pixels = surface->pixels;
 
-	for (int i = 0; i < h; i++)
+  for (int i = 0; i < h; i++)
+  {
+	for (int j = 0; j < w; j++)
 	{
-		for (int j = 0; j < w; j++)
-		{
-      Uint8 r, g, b;
-      SDL_GetRGB(pixels[i*(w)+j], format, &r, &g, &b);
+      		Uint8 r, g, b;
+      		SDL_GetRGB(pixels[i*(w)+j], format, &r, &g, &b);
       
-      Uint8 r1, g1, b1;
-      SDL_GetRGB(pixels[i*(w)+j+1], format, &r1, &g1, &b1);
+      		Uint8 r1, g1, b1;
+      		SDL_GetRGB(pixels[i*(w)+j+1], format, &r1, &g1, &b1);
       
-      Uint8 r2, g2, b2;
-      SDL_GetRGB(pixels[(i+1)*(w)+j], format, &r2, &g2, &b2);
+      		Uint8 r2, g2, b2;
+      		SDL_GetRGB(pixels[(i+1)*(w)+j], format, &r2, &g2, &b2);
       
-			reset_visited(surface);
-			COUNT = 0;
+		reset_visited(surface, visited);
+		COUNT = 0;
 
 			// checking cell to the right
-			if (j + 1 < w)
-			{
-				BFS(r, r1, i, j, surface);
-			}
+		if (j + 1 < w)
+		{
+			BFS(r, r1, i, j, surface, visited);
+		}
 
 			// updating result
-			if (COUNT >= current_max)
-			{
-				current_max = COUNT;
-				reset_result(surface);
-			}
-			reset_visited(surface);
-			COUNT = 0;
+		if (COUNT >= current_max)
+		{
+			current_max = COUNT;
+			reset_result(r ,surface, visited, result);
+		}
+		reset_visited(surface, visited);
+		COUNT = 0;
 
 			// checking cell downwards
-			if (i + 1 < h)
-			{
-				BFS(r, r2, i, j, surface);
-			}
+		if (i + 1 < h)
+		{
+			BFS(r, r2, i, j, surface, visited);
+		}
 
 			// updating result
-			if (COUNT >= current_max)
-			{
-				current_max = COUNT;
-				reset_result(surface);
-			}
+		if (COUNT >= current_max)
+		{
+			current_max = COUNT;
+			reset_result(r, surface, visited, result);
 		}
+	}
+   }
   
-  	int w1 = 0;
-  	int h1 = 0;
+    int w1 = 0;
+    int h1 = 0;
     int h2 = 0;
-  	int w2 = 0;
-  	bool b1 = true;
-    bool b2 = true;
-  	for (int i = 0; i < n; i++)
-	  {
-		  for (int j = 0; j < m; j++)
-		  {
-          if (result[i*w+j] == 1 && b1)
+    int w2 = 0;
+    int b1 = 1;
+    int b2 = 1;
+    for (int i = 0; i < h; i++)
+    {
+    	for (int j = 0; j < w; j++)
+	{
+          if (result[i*w+j] == 1 && b1 == 1)
           {
              h1 = i;
-             b1 = false;
+             b1 = 0;
           }
           if (result[i*w+j] == 1)
           {
@@ -199,19 +204,19 @@ void grid_coordinates(SDL_Surface* surface, int[] values)
         }
     }
   
-  	for (int i = 0; i < m; i++)
-	  {
-		for (int j = 0; j < n; j++)
-		{
-          	if (result[i*w+j] == 1 && b2)
-            {
+    for (int i = 0; i < w; i++)
+    {
+	for (int j = 0; j < h; j++)
+	{
+          if (result[i*w+j] == 1 && b2 == 1)
+          {
               	w1 = i;
-              	b2 = false;
-            }
-          	if (result[i*w+j] == 1)
-            {
+              	b2 = 0;
+          }
+          if (result[i*w+j] == 1)
+          {
               	w2 = i;
-            }
+          }
         }
     }
     values[0] = h1;
