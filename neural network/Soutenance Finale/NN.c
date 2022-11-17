d5f8bb17447c08ef6d41e78562208ce6caeae5ea
G474XY <paul.pazart@epita.fr>
2022-11-17 21:30:47 +0100

    Beginning of NN
    Types def and beginning of an init function

--- neural network/Soutenance Finale/NN.c
+++ neural network/Soutenance Finale/NN.c
@@ -1,244 +1,127 @@
-//
-// Created by ppaza on 24/06/2022.
-// Source: https://www.youtube.com/playlist?list=PLO_fdPEVlfKoanjvTJbIbd9V5d9Pzp8Rw
-// Note : pas sûr de la valeur pour les boucles => à refaire
-//
-
 #include "NN.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define numInputs 2
-#define numHiddenLayer 1
-#define numHiddenNodes 1
+#define nbInput 28*28
 
-#define numOutputNodes 0
+#define nbILayer 1
+#define nbHLayer 3
+#define nbOLayer 1
 
-#define numTrainingSets 4
+#define nbHNode1 784
+#define nbHNode2 392
+#define nbHNode3 196
+#define nbONode 1
 
+#define MAX(x,y) ((x)>(y)?(x):(y))
 
-//Stock A = sigmoid(- X*W+B)
-double hiddenLayer[numHiddenLayer][numHiddenNodes];
-double outputLayer[numOutputNodes];
-
-//Stock B
-double hiddenLayerBias[numHiddenLayer][numHiddenNodes];
-double outputLayerBias[numOutputNodes];
-
-//Stock W
-double hiddenWeight[numHiddenLayer][numHiddenNodes][numInputs];
-double outputWeight[numHiddenNodes][numOutputNodes];
-
-
-//+-+-+-+-+-+-+-+-+Fonctions pour neurones+-+-+-+-+-+-+-+-+
-double sigmoid(double z) {
-    return 1 / (1 + exp(-z));
-}
-
-//Calcule la sortie du neurone
-double model(double x[], double w[], double b) {
-    double z = 0;
-    for (int i = 0; i < numInputs; ++i) {
-        z += w[i] * x[i];
+double getmax(double* a, int len) {
+    double max = a[0];
+    for (int i = 0; i < a; i++) {
+        max = a[i] > max ? a[i] : max;
     }
-    z += b;
-    return sigmoid(z);
+    return max;
 }
 
-//Calcule l'erreur entre la sortie et ce qui été attendu
-double log_loss(double a[], double y[]){
-    double l = 0;
-
-        for (int i = 0; i < numTrainingSets; ++i) {
-            l += y[i] * log(a[i]) + (1 - y[i]) * log(1 - a[i]);
-        }
-
-        return -(1/numTrainingSets) * l;
+void LeakyReLU(neuron *neuron, double z, double alpha) {
+    neuron->value = MAX(alpha * z, z);
 }
 
-//Calcule de la descente de gradient
-void gradients(double a[], double x[], double y[], double *dw, double *db){
-    *dw = 0;
-    *db = 0;
-    for (int i = 0; i < numTrainingSets; ++i) {
-        *dw += x[i] * (a[i] - y[i]);
-        *db += a[i] - y[i];
+void dsLeakyReLU(neuron *neuron, double z, double alpha) {
+    neuron->value = z > 0 ? 1 : alpha;
+}
+
+double* softmax(double* input, int len){
+    double res[len];
+    double t = getmax(input, len);
+
+    for (int i = 0; i < len; i++) {
+        res[i] = exp(input[i] - t);
     }
 
-    *db *= 1/numTrainingSets;
-    *dw *= 1/numTrainingSets;
+    double sumexp = 0;
+    for (int i = 0; i < len; ++i) {
+        sumexp += res[i];
+    }
+
+    for (int i = 0; i < len; ++i) {
+        res[i] = res[i] / sumexp;
+    }
+
+    return res;
 }
 
-void update(double dw, double db, double *w[], double *b, int learning_rate){
+void dssoftmax(){}
 
-    for (int i = 0; i < numInputs; ++i)
-        *w[i] = *w[i] - learning_rate * dw;
+void forwardpass(){}
 
-    *b = *b - learning_rate * db;
-}
+void backwardpass(){}
 
-//+-+-+-+-+-+-+-+-+Fonctions pour réseau+-+-+-+-+-+-+-+-+
+void shuffle(){}
 
+layer* init(){
+    layer* a = malloc(sizeof(layer) * (nbILayer + nbHLayer + nbOLayer));
+    long n = 0;
 
-//Fonctions d'initialisation
-double random() {
-    return ((double) rand() / (double) RAND_MAX);
-}
-
-void initialisation()
-{
-    // initialisation hidden layer
-    for (int i = 0; i < numHiddenLayer; ++i) {
-        for (int j = 0; j < numHiddenNodes; ++j) {
-            for (int k = 0; k < numInputs; ++k) {
-                hiddenWeight[i][j][k] = random();
-            }
-            hiddenLayerBias[i][j] = random();
-            hiddenLayer[i][j] = random();
+    for (long j = 0; j < nbInput; ++j) {
+        a[n].array[j].bias = 0; //random value ---------------------------------------------
+        a[n].array[j].value = 0;
+        a[n].array[j].weight = malloc(sizeof(double) * nbInput);
+        for (long k = 0; k < nbInput; ++k) {
+            a[n].array[j].weight[k] = 0; //random value ---------------------------------------------
         }
     }
+    n++;
 
-    // initialisation output layer avec un seul layer de sortie
-    for (int i = 0; i < numHiddenNodes; ++i) {
-        for (int j = 0; j < numOutputNodes; ++j) {
-            outputWeight[i][j] = random();
+    //------------------------------- 1e couche hidden -------------------------------
+    a[n].length = 0;
+    a[n].array = malloc(sizeof(neuron) * nbHNode1);
+
+    for (long j = 0; j < nbHNode1; ++j) {
+        a[n].array[j].bias = 0; //random value ---------------------------------------------
+        a[n].array[j].value = 0;
+        a[n].array[j].weight = malloc(sizeof(double) * nbInput);
+        for (long k = 0; k < nbInput; ++k) {
+            a[n].array[j].weight[k] = 0; //random value ---------------------------------------------
         }
-        outputLayerBias[i] = random();
-        outputLayer[i] = random();
     }
+    n++;
+
+    for (long i = 1; i < nbHLayer; ++i) {
+        a[n].length = 0;
+        a[n].array = malloc(sizeof(neuron) * nbHNode1);
+        for (long j = 0; j < nbInput; ++j) {
+            a[n].array[j].bias = 0; //random value ---------------------------------------------
+            a[n].array[j].value = 0;
+            a[n].array[j].weight = malloc(sizeof(double) * nbInput);
+            for (long k = 0; k < nbInput; ++k) {
+                a[n].array[j].weight[k] = 0; //random value ---------------------------------------------
+            }
+        }
+        n++;
+    }
+
+    //------------------------------- couche output -------------------------------
+
+    a[n].length = 0;
+    a[n].array = malloc(sizeof(neuron) * nbONode);
+
+    a[n].array[0].bias = 0; //random value ---------------------------------------------
+    a[n].array[0].value = 0;
+    a[n].array[0].weight = malloc(sizeof(double) * nbHNode1);
+    for (long k = 0; k < nbInput; ++k) {
+        a[n].array[0].weight[k] = 0; //random value ---------------------------------------------
+    }
+
+    return a;
 }
 
-void shuffle(int *array, size_t n){
-    if (n > 1) {
-        for (size_t i = 0; i < n - 1; ++i) {
-            size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-            int t = array[j];
-            array[j] = array[i];
-            array[i] = t;
-        }
-    }
-}
++int main(){
 
-//Forward propagation
-
-// si Training
-    //Back propagation
-
-    //Update
-
-//+-+-+-+-+-+-+-+-+Fonctions pour l'exploitation+-+-+-+-+-+-+-+-+
-
-int main(){
-
-    const double learning_rate = 0.1f;
-
-    // Faire un malloc
-    //Stock A = sigmoid(- X*W+B)
-    double hiddenLayer[numHiddenLayer][numHiddenNodes];
-    double outputLayer[numOutputNodes];
-
-    //Stock B
-    double hiddenLayerBias[numHiddenLayer][numHiddenNodes];
-    double outputLayerBias[numOutputNodes];
-
-    //Stock W
-    double hiddenWeight[numHiddenLayer][numHiddenNodes][numInputs];
-    double outputWeight[numHiddenNodes][numOutputNodes];
-
-    
-    double training_inputs[numTrainingSets][numInputs] = {{0.0f, 0.0f},
-                                                          {1.0f, 0.0f},
-                                                          {0.0f, 1.0f},
-                                                          {1.0f, 1.0f}};
-
-    double training_outputs[numTrainingSets][numOutputNodes] = {{0.0f},
-                                                                {1.0f},
-                                                                {1.0f},
-                                                                {0.0f}};
-
-    int trainingSetOrder[] = {0, 1, 2, 3};
-
-    initialisation();
-
-    int numberOfEpochs = 1000;
-
-    // Train the neural network for a number of epoch
-    for (int epoch = 0; epoch < numberOfEpochs; ++epoch) {
-
-        shuffle(trainingSetOrder, numTrainingSets);
-
-        for (int x = 0; x < numTrainingSets; ++x) {
-            int i = trainingSetOrder[x];
-
-            //Forward pass
-
-            //Compute hidden layer activation
-            for (int j = 0; j < numHiddenLayer; ++j) {
-                for (int k = 0; k < numHiddenNodes; ++j) {
-                    double activation = hiddenLayerBias[j][k];
-                    for (int l = 0; l < numInputs; ++l) {
-                        activation += training_inputs[i][l] * hiddenWeight[j][k][l];
-                    }
-                    hiddenLayer[j][k] = sigmoid(activation);
-            }
-
-
-            //Compute output layer activation
-                for (int j = 0; j < numOutputNodes; ++j) {
-                    double activation = hiddenLayerBias[j];
-                    for (int k = 0; k < numHiddenNodes; ++k) {
-                        activation += hiddenLayer[k] * outputWeight[k][j];
-                    }
-                    outputLayer[j] = sigmoid(activation);
-                }
-
-            // Print result from forward pass
-            printf("Input: %g   Output : %g   Predicted Output: %g  \n",
-                   training_inputs[i][0], training_inputs[i][1], outputLayer[0], training_outputs[i][0]);
-
-            // Backpropagation
-
-            // Compute change in ouput weights
-            double deltaOutput[numOutputNodes];
-
-            for (int j = 0; j < numOutputNodes; ++j) {
-                double error = (training_outputs[i][j] - outputLayer[j]);
-                deltaOutput[j] = error * dSigmoid(outputLayerBias[j]);
-            }
-
-            // Compute change in hidden weights
-            double deltaHidden[numHiddenNodes];
-
-            for (int j = 0; j < numHiddenNodes; ++j) {
-                double error = 0.0f;
-                for (int k = 0; k < numOutputNodes; ++k) {
-                    error += deltaOutput[k] * outputWeight[j][k];
-                }
-                deltaHidden[j] = error * dSigmoid(hiddenLayer[j]);
-            }
-
-            // Apply change in output weights
-            for (int j = 0; j < numOutputNodes; ++j) {
-                outputLayerBias[j] += deltaOutput[j] * learning_rate;
-                for (int k = 0; k < numHiddenNodes; ++k) {
-                    outputWeight[k][j] += hiddenLayer[k] * deltaOutput[j] * learning_rate;
-                }
-            }
-
-            // Apply change in hidden weights
-            for (int j = 0; j < numHiddenNodes; ++j) {
-                hiddenLayerBias[j] += deltaHidden[j] * learning_rate;
-                for (int k = 0; k < numHiddenNodes; ++k) {
-                    hiddenWeight[k][j] += training_inputs[i][k] * deltaHidden[j] * learning_rate;
-                }
-            }
-
-
-        }
-
-    }
+    //const double learning_rate = 0.1f;
+    layer * a = init();
+    printf("Hello World");
 
     return 0;
 }
-
--- neural network/Soutenance Finale/NN.h
+++ neural network/Soutenance Finale/NN.h
@@ -5,4 +5,17 @@
 #ifndef OCR_NN_H
 #define OCR_NN_H
 
+typedef struct neuron neuron;
+struct neuron{
+    double* weight;
+    double bias;
+    double value;
+};
+
+typedef struct layer layer;
+struct layer{
+    neuron* array;
+    long length;
+};
+
 #endif //OCR_NN_H
