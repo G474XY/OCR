efb08c7d3c36835ae005b91ba80e8db16387af16
Bastien Gaulier <bgaulier35@gmail.com>
2022-11-20 15:01:48 +0100

    Finished saving BUT for double pointer system
    The saving/loading system is done for layer and network structs that use a double pointer as their arrays.
    I didn't change NN.h in this commit, though.

--- neural network/Soutenance Finale/NNsave.c
+++ neural network/Soutenance Finale/NNsave.c
@@ -10,16 +10,47 @@ void print_array(double* arr,size_t size)
     printf("]\n");
 }
 
-void print_neuron(neuron* neu,size_t w_size)
+void print_neuron(neuron* neu)
 {
     printf("{\nValue = %lf\nBias = %lf\nWeights = ",neu->value,neu->bias);
-    print_array(neu->weight,w_size);
+    print_array(neu->weight,neu->weight_length);
     printf("}\n");
+}
+
+void print_layer(layer* l)
+{
+    printf("[\nLength = %ld\n",l->length);
+    for(long i = 0; i < l->length; i++)
+        print_neuron((l->array[i]));
+    printf("]\n");
+}
+
+void print_network(network* n)
+{
+    printf("Number of layers = %ld\n",n->length);
+    for(long i = 0; i < n->length; i++)
+        print_layer(n->array[i]);
 }
 
 void free_neuron(neuron* n)
 {
     free(n->weight);
+    free(n);
+}
+
+void free_layer(layer* l)
+{
+    for(long i = 0; i < l->length; i++)
+        free_neuron((l->array[i]));
+    free(l->array);
+    free(l);
+}
+
+void free_network(network* n)
+{
+    for(long i = 0; i < n->length; i++)
+        free_layer(n->array[i]);
+    free(n->array);
     free(n);
 }
 
@@ -74,37 +105,149 @@ char doubleAfromfile(FILE *file,int size,double *array)
     return i < size;
 }
 
-neuron* load_neuron(FILE* file,size_t w_len)
+neuron* load_neuron(FILE* file)
 {
     if(read_char(file,'{'))
         return NULL;
     neuron* neu = malloc(sizeof(neuron));
     neu->value = doublefromfile(file);
     neu->bias = doublefromfile(file);
-    neu->weight = malloc(w_len * sizeof(double));
-    if(doubleAfromfile(file,w_len,neu->weight) || read_char(file,'}'))
+    neu->weight_length = doublefromfile(file);
+    neu->weight = malloc(neu->weight_length * sizeof(double));
+    if(doubleAfromfile(file,neu->weight_length,neu->weight)
+    || read_char(file,'}'))
     {
         printf("Error while reading neuron weights\n");
-        free(neu->weight);
-        free(neu);
+        free_neuron(neu);
         return NULL;
     }
     
     return neu;
 }
 
-//layer* load_layer(FILE* file)
-
-int main()
+layer* load_layer(FILE* file)
 {
-    FILE* file = fopen("save_tests/neuron.txt","r");
-    neuron* n = load_neuron(file,4);
+    if(read_char(file,'['))
+        return NULL;
+    layer* lay = malloc(sizeof(layer));
+    lay->length = doublefromfile(file);
+    lay->array = malloc(lay->length * sizeof(neuron*));
+    for(long i = 0; i < lay->length; i++)
+    {
+        neuron* n = load_neuron(file);
+        if(n == NULL)
+        {
+            printf("Error while loading neuron of layer\n");
+            free_layer(lay);
+            return NULL;
+        }
+        lay->array[i] = n;
+    }
+    if(read_char(file,']'))
+    {
+        printf("Error while ending layer\n");
+        free_layer(lay);
+        return NULL;
+    }
+
+    return lay;
+}
+
+network* load_network_error(network* net)
+{
+    free_network(net);
+    return NULL;
+}
+
+network* load_network(FILE* file)
+{
+    if(read_char(file,'('))
+        return NULL;
+    network* net = malloc(sizeof(network));
+    net->length = doublefromfile(file);
+    net->array = malloc(net->length * sizeof(layer*));
+    for(long i = 0; i < net->length; i++)
+    {
+        layer* l = load_layer(file);
+        if(l == NULL)
+            return load_network_error(net);
+        net->array[i] = l;
+    }
+    if(read_char(file,')'))
+        return load_network_error(net);
+    return net;
+}
+
+//========================================
+
+//=================SAVING=================
+
+void doubletofile(double d, FILE *file)
+{
+    fprintf(file,"%f",d);
+}
+
+void doubleAtofile(double *array,int size,FILE *file)
+{
+    fprintf(file,"%s","[ ");
+    for(int i = 0; i < size; i++)
+    {
+        doubletofile(array[i],file);
+        fprintf(file,"%c",' ');
+    }
+    fprintf(file,"%s","]\n");
+}
+
+void save_neuron(neuron* neu,FILE* file)
+{
+    fprintf(file,"{\n");
+    doubletofile(neu->value,file);
+    fprintf(file,"\n");
+    doubletofile(neu->bias,file);
+    fprintf(file,"\n");
+    doubletofile(neu->weight_length,file);
+    fprintf(file,"\n");
+    doubleAtofile(neu->weight,neu->weight_length,file);
+    fprintf(file,"}\n");
+}
+
+void save_layer(layer* lay,FILE* file)
+{
+    fprintf(file,"[\n");
+    doubletofile(lay->length,file);
+    fprintf(file,"\n");
+    for(long i = 0; i < lay->length; i++)
+        save_neuron(lay->array[i],file);
+    fprintf(file,"]\n");
+}
+
+void save_network(network* net,FILE* file)
+{
+    fprintf(file,"(\n");
+    doubletofile(net->length,file);
+    fprintf(file,"\n");
+    for(long i = 0; i < net->length; i++)
+        save_layer(net->array[i],file);
+    fprintf(file,")\n");
+}
+
+//========================================
+
+/*int main()
+{
+    FILE* file = fopen("save_tests/network.txt","r");
+    network* n = load_network(file);
     if(n == NULL)
-        printf("Error while loading neuron\n");
+        printf("Error while loading neunetworkron\n");
     else
     {
-        print_neuron(n,4);
-        free_neuron(n);
+        print_network(n);
+        fclose(file);
+        file = fopen("save_tests/network_save.txt","w");
+        save_network(n,file);
+        fclose(file);
+        free_network(n);
     }
+
     return 0;
-}
+}*/
