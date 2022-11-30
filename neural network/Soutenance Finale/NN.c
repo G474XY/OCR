05dae24314f1f0ff51a9945940d6afa9447e952f
G474XY <paul.pazart@epita.fr>
2022-11-30 22:46:52 +0100

    Backprop input layer and free remaining

--- neural network/Soutenance Finale/NN.c
+++ neural network/Soutenance Finale/NN.c
@@ -49,8 +49,8 @@ double Z2(neuron neuron, double* input){
     return res + neuron.bias;
 }
 
-void dsLeakyReLU(neuron *neuron, double z, double alpha) {
-    neuron->value = z > 0 ? 1 : alpha;
+double dsLeakyReLU(double z, double alpha) {
+    return z > 0 ? 1 : alpha;
 }
 
 double softmax(double value, double sum){
@@ -58,28 +58,38 @@ double softmax(double value, double sum){
 }
 
 //Result array of softmax function and indice of the current calc value from softmax too
-double dssoftmax(double* a, long i, long j, double sum){
+double dssoftmax(double a, double partial_sum, double sum){
 
-    if(i == j)
-        return a[i] * (1 - a[j]);
-    else
-        return -a[i], sum * a[j];
-
+    return (a * partial_sum) / (sum * sum);
 }
 
 //Log Loss cost function
-double cost(double* get, double* expected){
-    double res = 0;
+double* cost(double* get, double* expected){
+    double res[nbONode];
     for (int i = 0; i < nbONode; ++i) {
-        res += get[i] * log(expected[i])
-                + (1 - get[i]) * log(1 - expected[i]);
+        res[i] = -1 * (get[i] * log(expected[i])
+                + (1 - get[i]) * log(1 - expected[i]));
     }
-    res *= (1./nbONode);
+
     return res;
 }
 
-void dscost(){
+double average(double* a){
 
+    double res = 0;
+    for (int i = 0; i < nbONode; ++i) {
+        res += a[i];
+    }
+    return (1/nbONode) * res;
+}
+
+double partial_sum(layer* a, int except){
+    double res = 0;
+    for (int i = 0; i < nbONode; ++i) {
+        if(i != except)
+            res += a->array[i]->value;
+    }
+    return res;
 }
 
 double forwardpass(network a, double* input, double learning_rate){
@@ -89,8 +99,8 @@ double forwardpass(network a, double* input, double learning_rate){
         a.array[0]->array[i]->value = LeakyReLU(Z2(*a.array[0]->array[i], input), learning_rate);
     }
 
-    //Hidden Layer & Output Layer
-    for (int i = 1; i < a.length; ++i) {
+    //Hidden Layer
+    for (int i = 1; i < a.length - 1; ++i) {
         for (int j = 0; j < a.array[i]->length; ++j) {
             a.array[i]->array[j]->value = LeakyReLU(Z1(*a.array[i]->array[j], *a.array[i-1]->array), learning_rate);
         }
@@ -101,66 +111,75 @@ double forwardpass(network a, double* input, double learning_rate){
     //sum for softmax
     double sum = 0;
     for (long i = 0; i < a.array[a.length-1]->length; ++i) {
-        sum = exp(a.array[a.length-1]->array[i]->value);
+        sum += exp(Z1(*a.array[a.length-1]->array[i], *a.array[a.length - 2]->array));
     }
 
-    for (long i = 0; i < a.array[i]->length; ++i) {
-        a.array[a.length - 1]->array[i]->value = softmax(a.array[a.length - 1]->array[i]->value, sum);
+    for (long i = 0; i < a.array[i - 1]->length; ++i) {
+        a.array[a.length - 1]->array[i]->value = softmax(Z1(*a.array[a.length-1]->array[i],
+                                                            *a.array[a.length - 2]->array), sum);
     }
 
     return getmax(a.array[a.length-1]);
 }
 
-void backwardpass(network a, double* get, double* expected, double cost, double learning_rate){
-
-    //Output Layer
-
+void backwardpass(network a, double* get, double* expected, double* cost, double learning_rate){
+    
     //Sum of value
     double sum = 0;
-
     for (int i = 0; i < nbONode; ++i) {
         sum += get[i];
     }
 
-    //Qqchose avec error et dssoftmax
+    //Output Layer
 
-    for (int i = 0; i < a.array[a.length-1]->length; ++i) {
-
-        a.array[a.length-1]->array[i]->value = 0;//qqchose
-
-        for (long j = 0; j < a.array[a.length-1]->array[i]->weight_length; ++j) {
-            a.array[a.length-1]->array[i]->weight[j] = 0; //qqchose
-        }
-
-        a.array[a.length-1]->array[i]->bias = 0;//qqchose
+    //Compute dSoftmax
+    double* ds = malloc(a.array[a.length - 1]->length * sizeof(double));
+    for (int i = 0; i < a.array[a.length - 1]->length; i++) {
+        double ps = partial_sum(a.array[a.length - 1], i);
+        ds[i] = dssoftmax(a.array[a.length - 1]->array[i]->value, ps, sum);
     }
 
-    double* tmp;
+    //Compute dW, dB and apply change
+    for (int i = 0; i <  a.array[a.length - 1]->length; ++i) {
+
+        a.array[a.length - 1]->array[i]->bias -= learning_rate * ds[i] * cost[i];
+        a.array[a.length - 1]->array[i]->value = ds[i];
+
+        for(int j = 0; j < a.array[a.length - 1]->array[i]->weight_length; ++j) {
+            a.array[a.length - 1]->array[i]->weight[j] -= learning_rate *
+                    (a.array[a.length - 2]->array[j]->value * ds[i] * cost[i]);
+        }
+    }
 
     //Hidden Layer
-    for (int i = a.length-2; i > -1; ++i) {
 
-        // Compute change in hidden weights
-        double deltaHidden[a.array[i]->length];
-        for (int j=0; j < a.array[i]->length; j++) {
-            double errorHidden = 0.0f;
-            for(int k=0; k < a.array[i+1]->length; k++) {
-                errorHidden += tmp[k] * a.array[i+1]->array[k]->weight[j];
+    for (int i = a.length - 2; i > 0; i--) {
+
+        //Compute dW, dB and apply change
+        double* E = malloc(a.array[i]->length);
+
+        for (int j = 0; j < a.array[i]->length; ++j) {
+            for (int k = 0; k < a.array[i+1]->length; ++k) {
+                E[j] += cost[k] * a.array[i+1]->array[k]->value * a.array[i+1]->array[k]->weight[j];
             }
-            deltaHidden[j] = errorHidden * dsLeakyReLU(a.array[i]->array[j], ? ,0.01);
         }
 
         //Apply change
-        for (int j = 0; j < a.array[i]->length; ++j) {
-            //a.array[i]->array[j]->value = deltaHidden[j] * learning_rate;//qqchose
+        for (int j = 0; j <  a.array[i]->length; j++) {
 
-            for (long k = 0; j < a.array[i]->array[j]->weight_length; ++j) {
-                a.array[i]->array[j]->weight[k] = deltaHidden[j] * learning_rate; //?
+            a.array[i]->array[j]->bias -= learning_rate * dsLeakyReLU(a.array[i]->array[j]->value, 0.01) * E[j];
+
+            for(int k = 0; k < a.array[i]->array[j]->weight_length; k++) {
+                a.array[i]->array[j]->weight[k] -= learning_rate *
+                        (dsLeakyReLU(a.array[i]->array[j]->value, 0.01) * E[j] * a.array[i - 1]->array[j]->value);
             }
-
-            a.array[i]->array[j]->bias = deltaHidden[j] * learning_rate;//qqchose
         }
+
     }
+
+    //Input Layer
+
+
 
 }
 
@@ -195,11 +214,12 @@ void training(network* network, double*** input, long epoch, double learning_rat
         }
 
         //Calc error
-        double error = cost(tmp, (double *) expected[indexe]);
+        double* c = cost(tmp, (double *) expected[indexe]);
+        double error = average(c);
         printf("Enter: %d      Get: %f       The margin of error: %f\n", ((indexe + 1) % 10), res, (1 - error * 100));
 
         //Backward
-        backwardpass(*network, tmp, (double *)expected[indexe]);
+        backwardpass(*network, tmp, (double *)expected[indexe], error, learning_rate);
     }
 
 }
