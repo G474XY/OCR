b5945f7ba1e164bcd2aa6ba218f6d4115536e419
Jacques2904 <97622822+Jacques2904@users.noreply.github.com>
2022-12-08 00:05:16 +0100

    Update apply_image.c

--- imageTreatment/apply_image.c
+++ imageTreatment/apply_image.c
@@ -12,10 +12,10 @@
 void load_image(const char* path)
 {
     SDL_Surface* tmp = IMG_Load(path);
-    if (s1 == NULL)
+    if (tmp == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
     SDL_Surface* s = SDL_ConvertSurfaceFormat(tmp, SDL_PIXELFORMAT_RGB888, 0);
-    if (tmp == NULL)
+    if (s == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     SDL_FreeSurface(tmp);
